47aa01efb7a28ba5d8e9ffbfee13d234a9fb3603
Callum Walley <callum.walley@nesi.org.nz>
2026-04-28 14:55:00 +1200

    Animate in plot single partition

--- examples/single_partition.py
+++ examples/single_partition.py
@@ -16,38 +16,41 @@ def main(*, mapname: str='FlowFM_0000_map.nc', start_time: int=0, end_time: int=
     bedlevelt0 = ugrid._readField('mesh2d_s1', 0) - ugrid._readField('mesh2d_dg', 0)
 
     pl = pv.Plotter(shape=(1, 2))
-    polymesh = ugrid._buildVTKPolyData()
+    # Create separate mesh objects for each subplot
+    mesh_waterdepth = ugrid._buildVTKPolyData().copy()
+    mesh_dod = ugrid._buildVTKPolyData().copy()
+
+
+    # Set initial cell data for dod
+    mesh_dod.cell_data["dod"] = bedlevelt0
 
     # Add initial meshes to each subplot
     pl.subplot(0, 0)
-    waterdepth = pl.add_mesh(polymesh, scalars=ugrid._readField('mesh2d_waterdepth', 0), name="waterdepth")
+    pl.add_mesh(mesh_waterdepth, scalars=ugrid._readField('mesh2d_waterdepth', 0), name="waterdepth")
     pl.subplot(0, 1)
-    dod = pl.add_mesh(polymesh, scalars=np.zeros_like(bedlevelt0), name="dod")
+    pl.add_mesh(mesh_dod, scalars="dod", name="dod")
+    pl.show(auto_close=False)
 
-    pl.show(auto_close=False)  # Keep window open
 
     # Determine the number of timesteps
     end_time = len(ugrid.time) if end_time is None else end_time
     
     time_indices = range(start_time, end_time)
 
-    # Set initial title for the first subplot only
-    pl.subplot(0, 0)
-    pl.add_title(f"T: {start_time}", color='black')
-
     for time_index in time_indices:
         bedlevel = ugrid._readField('mesh2d_s1', time_index) - ugrid._readField('mesh2d_dg', time_index)
         dod = bedlevel - bedlevelt0
 
-        # Update cell data on the mesh directly
-        polymesh.cell_data["waterdepth"] = ugrid._readField('mesh2d_waterdepth', time_index)
-        polymesh.cell_data["dod"] = dod
+        # Update cell data on each mesh independently
+        mesh_waterdepth.cell_data["waterdepth"] = ugrid._readField('mesh2d_waterdepth', time_index)
+        mesh_dod.cell_data["dod"] = dod
 
         # Update title for the first subplot only
         pl.subplot(0, 0)
-        pl.add_title(f"t={time_index}", color='black')
+        pl.add_title(f"t={time_index}/{end_time}", color='black')
 
         pl.render()
+        
         time.sleep(0.1)
 
     pl.close()
