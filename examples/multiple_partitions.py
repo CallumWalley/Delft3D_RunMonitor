17c5971a9f9bceef03408218e6aceabed377592a
Callum Walley <callum.walley@nesi.org.nz>
2026-04-28 15:05:14 +1200

    mulytiplotting

--- examples/multiple_partitions.py
+++ examples/multiple_partitions.py
@@ -1,16 +1,17 @@
 from Delft3D_RunMonitor.multi_ugrid_mesh import MultiUGridMesh
-import defopt
 import pyvista as pv
+import defopt
+from netCDF4 import Dataset
+import time
 import numpy as np
-
 
 from glob import glob
 
-def main(*, mappattern: str='FlowFM_*_map.nc', time_index: int=0, cmin: float=None, cmax: float=None):
+def main(*, mappattern: str='FlowFM_*_map.nc', start_time: int=0, end_time: int=None, cmin: float=None, cmax: float=None):
     """
     mappattern: glob pattern for the map files
-    varname: variable name
-    time_index: time index
+    start_time: first frame to plot
+    end_time: last frame to plot (exclusive, default: all)
     """
     mapnames = glob(mappattern)
     ugrid = MultiUGridMesh(mapnames)
@@ -18,24 +19,38 @@ def main(*, mappattern: str='FlowFM_*_map.nc', time_index: int=0, cmin: float=No
     if type(cmin) is float and type(cmax) is float:
         clim = [float(cmin), float(cmax)]
 
-    bedlevel_t0 = np.concatenate([m._readField('mesh2d_s1', 0) - m._readField('mesh2d_dg', 0) for m in ugrid.meshes])
-    bedlevel_t = np.concatenate([m._readField('mesh2d_s1', time_index) - m._readField('mesh2d_dg', time_index) for m in ugrid.meshes])
-    dod = np.subtract(bedlevel_t, bedlevel_t0)
+    end_time = len(ugrid.meshes[0].time) if end_time is None else end_time
 
+
+    bedlevel_t0 = np.concatenate([m._readField('mesh2d_s1', 0) - m._readField('mesh2d_dg', 0) for m in ugrid.meshes])
 
     polymesh = pv.merge([m._buildVTKPolyData() for m in ugrid.meshes])
     polymesh.cell_data["waterdepth"] = np.concatenate([ m._readField('mesh2d_waterdepth', 0) for m in ugrid.meshes ])
-    polymesh.cell_data["dod"] = dod
+    polymesh.cell_data["dod"] = np.zeros_like(bedlevel_t0)
 
     pl = pv.Plotter(shape=(1, 2))
     pl.subplot(0, 0)
     pl.add_mesh(polymesh, scalars="waterdepth", clim=clim)
-    
     pl.subplot(0, 1)
     pl.add_mesh(polymesh, scalars="dod", clim=clim)
 
+    pl.show(auto_close=False)
 
-    pl.show()
+    for time_index in range(start_time, end_time):
+        bedlevel_t = np.concatenate([m._readField('mesh2d_s1', time_index) - m._readField('mesh2d_dg', time_index) for m in ugrid.meshes])
+        dod = np.subtract(bedlevel_t, bedlevel_t0)
+        polymesh.cell_data["waterdepth"] = np.concatenate([ m._readField('mesh2d_waterdepth', time_index) for m in ugrid.meshes ])
+        polymesh.cell_data["dod"] = dod
+
+        pl.subplot(0, 0)
+        pl.add_title(f"t={time_index}/{end_time}", color='black')
+        pl.render()
+        import time
+        time.sleep(0.1)
+
+    pl.close()
 
 if __name__ == '__main__':
+    import sys
+    import defopt
     defopt.run(main)
