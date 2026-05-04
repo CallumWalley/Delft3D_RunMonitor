21773c8d7b5c85779afaed2c745c829089cb149b
Callum Walley <callum.walley@nesi.org.nz>
2026-05-04 16:20:21 +1200

    updated init.py

--- Delft3D_RunMonitor/__init__.py
+++ Delft3D_RunMonitor/__init__.py
@@ -1,4 +1,5 @@
 from .ugrid_mesh import UGridMesh
 from .multi_ugrid_mesh import MultiUGridMesh
+from .plot_utils import load_cross_sections, add_xs_overlay
 
-__all__ = ["MultiUGridMesh", "UGridMesh"]
+__all__ = ["MultiUGridMesh", "UGridMesh", "load_cross_sections", "add_xs_overlay"]
