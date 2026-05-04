55332277830473f75b46c3de0d645e279f91388b
Callum Walley <callum.walley@nesi.org.nz>
2026-05-04 16:28:04 +1200

    some plotting improvemnts. added plotting cross sectiosn

--- examples/multiple_partitions.py
+++ examples/multiple_partitions.py
@@ -1,17 +1,18 @@
-from Delft3D_RunMonitor import MultiUGridMesh
+from Delft3D_RunMonitor import MultiUGridMesh, load_cross_sections, add_xs_overlay
 import defopt
-from netCDF4 import Dataset
 import time
 import numpy as np
 import pyvista as pv
 
 from glob import glob
 
-def main(*, mappattern: str='FlowFM_*_map.nc', start_time: int=0, end_time: int=None, cmin: float=None, cmax: float=None):
+
+def main(*, mappattern: str='FlowFM_*_map.nc', start_time: int=0, end_time: int=None, cmin: float=None, cmax: float=None, xs_file: str=None):
     """
     mappattern: glob pattern for the map files
     start_time: first frame to plot
     end_time: last frame to plot (exclusive, default: all)
+    xs_file: path to cross-section point-pair file (optional)
     """
     mapnames = glob(mappattern)
     ugrid = MultiUGridMesh(mapnames)
@@ -30,12 +31,18 @@ def main(*, mappattern: str='FlowFM_*_map.nc', start_time: int=0, end_time: int=
 
     scalar_bar_args = {'vertical': True, 'position_x': 0.9, 'position_y': 0.05, 'width': 0.05, 'height': 0.9}
 
+    xs_mesh = load_cross_sections(xs_file) if xs_file else None
+
     pl = pv.Plotter(shape=(1, 2))
     pl.subplot(0, 0)
     pl.add_mesh(polymesh, scalars="waterdepth", clim=clim or [0, 2], scalar_bar_args=scalar_bar_args)
+    if xs_mesh:
+        add_xs_overlay(pl, xs_mesh)
 
     pl.subplot(0, 1)
     pl.add_mesh(polymesh, scalars="dod", clim=clim or [-2, 2], cmap="bwr", scalar_bar_args=scalar_bar_args)
+    if xs_mesh:
+        add_xs_overlay(pl, xs_mesh)
 
     pl.show(auto_close=False)
 
--- examples/single_partition.py
+++ examples/single_partition.py
@@ -1,33 +1,38 @@
-from Delft3D_RunMonitor import UGridMesh
+from Delft3D_RunMonitor import UGridMesh, load_cross_sections, add_xs_overlay
 import defopt
-from netCDF4 import Dataset
 import time
 import numpy as np
+import pyvista as pv
 
 
-def main(*, mapname: str='FlowFM_0000_map.nc', start_time: int=0, end_time: int=None):
+def main(*, mapname: str='FlowFM_0000_map.nc', start_time: int=0, end_time: int=None, xs_file: str=None):
     """
     mapname: name of the map NetCDF file
-    varname: variable name
-    time_index: time index
+    start_time: first frame to plot
+    end_time: last frame to plot (exclusive, default: all)
+    xs_file: path to cross-section point-pair file (optional)
     """
     ugrid = UGridMesh(mapname)
     bedlevelt0 = ugrid.readField('mesh2d_s1', 0) - ugrid.readField('mesh2d_dg', 0)
 
-    pl = pv.Plotter(shape=(1, 2))
-    # Create separate mesh objects for each subplot
     mesh_waterdepth = ugrid._buildVTKPolyData().copy()
     mesh_dod = ugrid._buildVTKPolyData().copy()
+    mesh_dod.cell_data["dod"] = np.zeros_like(bedlevelt0)
 
+    scalar_bar_args = {'vertical': True, 'position_x': 0.9, 'position_y': 0.05, 'width': 0.05, 'height': 0.9}
+    xs_mesh = load_cross_sections(xs_file) if xs_file else None
 
-    # Set initial cell data for dod
-    mesh_dod.cell_data["dod"] = bedlevelt0
-
-    # Add initial meshes to each subplot
+    pl = pv.Plotter(shape=(1, 2))
     pl.subplot(0, 0)
-    pl.add_mesh(mesh_waterdepth, scalars=ugrid.readField('mesh2d_waterdepth', 0), name="waterdepth")
+    pl.add_mesh(mesh_waterdepth, scalars=ugrid.readField('mesh2d_waterdepth', 0), clim=[0, 1.5], scalar_bar_args=scalar_bar_args, name="waterdepth")
+    if xs_mesh:
+        add_xs_overlay(pl, xs_mesh)
+
     pl.subplot(0, 1)
-    pl.add_mesh(mesh_dod, scalars="dod", name="dod")
+    pl.add_mesh(mesh_dod, scalars="dod", clim=[-2, 2], cmap="bwr", scalar_bar_args=scalar_bar_args, name="dod")
+    if xs_mesh:
+        add_xs_overlay(pl, xs_mesh)
+
     pl.show(auto_close=False)
 
 
